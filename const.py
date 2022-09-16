82ad6753a4b8195e87bfbe7f9e449a6f422d746f
IanUFG <107971223+IanUFG@users.noreply.github.com>
2022-09-16 18:04:27 -0300

    Add files via upload

--- const.py
+++ const.py
@@ -1,5 +1,5 @@
-CHAT_SERVER_HOST = "ec2-44-203-59-102.compute-1.amazonaws.com"
+CHAT_SERVER_HOST = "ec2-3-86-106-71.compute-1.amazonaws.com"
 CHAT_SERVER_PORT = 5001
 
 # IDs de usuários registrados com os respectivos endereços IP e números de porta
-registry = {"Alice":("ec2-52-87-247-21.compute-1.amazonaws.com",5002), "Bob":("ec2-34-238-39-87.compute-1.amazonaws.com",5003), "Charlie":("ec2-34-238-39-87.compute-1.amazonaws.com", 5004)}
+registry = {"Alice":("ec2-3-91-230-94.compute-1.amazonaws.com",5002), "Bob":("ec2-52-55-144-212.compute-1.amazonaws.com",5003), "Charlie":("ec2-34-238-39-87.compute-1.amazonaws.com", 5004)}
